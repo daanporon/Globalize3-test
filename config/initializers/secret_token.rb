3f1c94952a60a62beae67541f0c2199f78dc6033
Daan Poron <daan.poron@gmail.com>
2011-10-30 23:47:17 +0100

    Empty rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Globalize3test::Application.config.secret_token = 'fd47e044cd7995963e00c8f7c6745e8c9753447a283fe53244f326494b73a7ab947f8be44c7d23fa449672ca28be13bb5afc6a0b124fac4911a8f300c24609a5'
